bd6e6d6b95cfa9c2aad089388e9ed1d6f43d9e27
Tolu Folorunso <tolufolorunso@yahoo.com>
2022-07-12 13:32:17 +0100

    Updated

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
 
 const {
   farmerRegistrationValidation,
